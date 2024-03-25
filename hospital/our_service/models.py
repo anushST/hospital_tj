354753132181eda102f278de36beef37f2f4bdf7
anushST <anushervon.s.06@gmail.com>
2024-03-25 14:56:11 +0500

    Add a lot of features and bug fix.

--- hospital/our_service/models.py
+++ hospital/our_service/models.py
@@ -1,4 +1,15 @@
-"""General project's models."""
+"""General project's models.
+
+Models:
+
+    Comment - Model for comments.
+
+    Hospital - Model for hospitals.
+
+    Rank - Model for ranks.
+
+    Service - Model for hospitals's services.
+"""
 from typing import Collection, Union
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
@@ -26,22 +37,7 @@ class HospitalServiceBaseModel(models.Model):
 
     name = models.CharField(max_length=64)
     description = models.TextField(max_length=512)
-    average_rank = models.FloatField(editable=False)
-
-    def save(self, *args, **kwargs) -> None:
-        """Save object to database."""
-        super().save(*args, **kwargs)
-        all_ranks = self.ranks.all()
-        if not all_ranks.count():
-            total_rank = sum(rank.value for rank in all_ranks)
-            self.average_rank = total_rank / len(all_ranks)
-        else:
-            self.average_rank = 0
-        super().save(*args, **kwargs)
-
-    def __str__(self) -> str:
-        """Return string to show when call str() method on current object."""
-        return self.name[:20]
+    average_rank = models.FloatField(default=0, editable=False, null=True)
 
     class Meta:
         """Meta data of the class."""
@@ -59,19 +55,20 @@ class Hospital(HospitalServiceBaseModel):
     "Mon-Fri 8:00-22:00(a break 12:00-13:00), Sat 8:00-20:00(...), Sun Closed".
         small_imgae (ImageField): The image with resolution 450x600.
         big_imgage (ImageField): The image with resoulution 1920x1080.
-        comments (ManyToManyField(Comment)): Hospital's comments.
-        ranks (ManyToManyField(Rank)): Hospital's ranks.
         average_rank (FloatField): The hospitals's average rank
     (note: calculates automaticly).
     """
 
-    work_time = models.TextField(max_length=256)
+    work_time = models.TextField(
+        max_length=256, help_text='Format: "Mon-Fri 8:00-22:00(a '
+        'break 12:00-13:00), Sat 8:00-20:00(...), Sun Closed"')
     small_image = models.ImageField(upload_to='hospital_images')
     big_image = models.ImageField(upload_to='hospital_images')
-    comments = models.ManyToManyField(
-        'Comment', related_name='comments_from_hospital')
-    ranks = models.ManyToManyField(
-        'Rank', related_name='ranks_from_hospital')
+
+    def __str__(self) -> str:
+        """Return string to show when call str() method on current object."""
+        average_rank = 10 if self.average_rank == 10 else self.average_rank
+        return f'{self.name[:20]} ({average_rank})'
 
 
 class Service(HospitalServiceBaseModel):
@@ -87,8 +84,6 @@ class Service(HospitalServiceBaseModel):
         max_price (FloatField): The max_price of the service
     (note: if price is constant put 0).
         hospital (ForeignKey(Hospital)): Service's hospital.
-        comments (ManyToManyField(Comment)): Service's comments.
-        ranks (ManyToManyField(Rank)): Service's ranks.
         average_rank (FloatField): The service's average rank
     (note: calculates automaticly).
     """
@@ -104,10 +99,6 @@ class Service(HospitalServiceBaseModel):
                                   default=0)
     hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE,
                                  related_name='hospital')
-    comments = models.ManyToManyField(
-        'Comment', related_name='comments_from_service')
-    ranks = models.ManyToManyField(
-        'Rank', related_name='ranks_from_service')
 
     def clean_fields(
         self,
@@ -131,6 +122,12 @@ class Service(HospitalServiceBaseModel):
                     'of fields: (price) or (min_price and max_price).'
                 )
 
+    def __str__(self) -> str:
+        """Return string to show when call str() method on current object."""
+        average_rank = 10 if self.average_rank == 10 else self.average_rank
+        return (f'{self.name[:20]} - {self.hospital.name[:20]} '
+                f'({average_rank})')
+
 
 class CommentRankBaseModel(models.Model):
     """Comment's and Rank's base model.
@@ -144,6 +141,32 @@ class CommentRankBaseModel(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def clean_fields(
+        self,
+        exclude: Union[Collection[str], None] = None
+    ) -> None:
+        """Validate fields."""
+        super().clean_fields(exclude)
+        if self.service is None and self.hospital is None:
+            raise ValidationError(
+                'Fields "service" and "hospital" are empty. '
+                'Please determine only one of them.'
+            )
+        if self.service is not None and self.hospital is not None:
+            raise ValidationError(
+                'You can not determine both fields "service" '
+                'and "hospital". Please determine only one of them.'
+            )
+
+    def __str__(self) -> str:
+        """Return string to show when call str() method on current object."""
+        if self.service is not None:
+            rest_info = (f'{self.service.name[:20]} '
+                         f'({self.service.hospital.name[:20]})')
+        else:
+            rest_info = f'{self.hospital.name[:20]}'
+        return f'{self.author.username[:20]} - {rest_info}'
 
     class Meta:
         """Meta data of the CommentRankBaseModel class."""
@@ -160,9 +183,21 @@ class Comment(CommentRankBaseModel):
         author (ForeignKey(User)): The author of the comment.
         created_at (DateTimeField): The time of comment's creation.
         updated_at (DateTimeField): The time of comment's updation.
+        service (ForeignKey(Service)): Comment's service
+    (note: if hospital_field is not blank leave this field blank).
+        hospital (ForeignKey(Hospital)): Comment's hospital
+    (note: if service_field is not blank leave this field blank).
     """
 
     text = models.TextField(help_text='Comment text')
+    service = models.ForeignKey(
+        Service, on_delete=models.CASCADE,
+        related_name='service_comment', null=True, blank=True,
+        help_text='if hospital_field is not blank leave this field blank')
+    hospital = models.ForeignKey(
+        Hospital, on_delete=models.CASCADE,
+        related_name='hospital_comment', null=True, blank=True,
+        help_text='if service_field is not blank leave this field blank')
 
 
 class Rank(CommentRankBaseModel):
@@ -173,10 +208,60 @@ class Rank(CommentRankBaseModel):
         author (ForeignKey(User)): The author of the rank.
         created_at (DateTimeField): The time of rank's creation.
         updated_at (DateTimeField): The time of rank's updation.
+        service (ForeignKey(Service)): Rank's service
+    (note: if hospital_field is not blank leave this field blank).
+        hospital (ForeignKey(Hospital)): Rank's hospital
+    (note: if service_field is not blank leave this field blank).
     """
 
     value = models.IntegerField(
         validators=[
             MinValueValidator(0, 'Rate can not be less than 0.'),
-            MaxValueValidator(10, 'Rate can not be more than 10')]
+            MaxValueValidator(10, 'Rate can not be more than 10'),
+        ]
     )
+    service = models.ForeignKey(
+        Service, on_delete=models.CASCADE,
+        related_name='service_rank', null=True, blank=True,
+        help_text='if hospital_field is not blank leave this field blank')
+    hospital = models.ForeignKey(
+        Hospital, on_delete=models.CASCADE,
+        related_name='hospital_rank', null=True, blank=True,
+        help_text='if service_field is not blank leave this field blank')
+
+    def save(self, *args, **kwargs) -> None:
+        """Save object to database."""
+        super().save(*args, **kwargs)
+        if self.service is not None:
+            all_ranks = Rank.objects.filter(service__id=self.service.id)
+        else:
+            all_ranks = Rank.objects.filter(hospital__id=self.hospital.id)
+        if all_ranks.count() != 0:
+            total_rank = sum(rank.value for rank in all_ranks)
+            average_rank = round(total_rank / all_ranks.count(), 1)
+        else:
+            average_rank = 0.0
+
+        if self.service is not None:
+            obj = Service.objects.get(id=self.service.id)
+            obj.average_rank = average_rank
+            obj.save()
+        else:
+            obj = Hospital.objects.get(id=self.hospital.id)
+            obj.average_rank = average_rank
+            obj.save()
+        super().save(*args, **kwargs)
+
+    class Meta:
+        """Meta data of the Rank class."""
+
+        constraints = (
+            models.UniqueConstraint(
+                name='unique_author_hospital',
+                fields=('author', 'hospital'),
+            ),
+            models.UniqueConstraint(
+                name='unique_author_service',
+                fields=('author', 'service'),
+            ),
+        )
